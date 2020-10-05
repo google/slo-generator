203ef97c8e2e768901745b0b6dc53f6171f29795
Olivier Cervello <ocervello@google.com>
2020-10-05 15:02:11 +0200

    remove unused imports

--- tests/unit/test_compute.py
+++ tests/unit/test_compute.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
 import unittest
-import sys
 import warnings
 
 from elasticsearch import Elasticsearch
