8eacf0eff34f13b38fc67c9a77f95bfb7b9f7b76
Olivier Cervello <ocervello@google.com>
2021-04-02 14:35:58 +0200

    Fix lint, add license

--- slo_generator/api/main.py
+++ slo_generator/api/main.py
@@ -1,17 +1,34 @@
+# Copyright 2021 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#            http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+`main.py`
+Functions Framework API (Flask).
+See https://github.com/GoogleCloudPlatform/functions-framework-python for
+details on the Functions Framework.
+"""
 import base64
 from os import environ
-import json
 import logging
 import pprint
-import time
-import yaml
 from datetime import datetime
-from urllib.parse import urlparse
+
+import yaml
+from flask import jsonify
 
 import google.cloud.storage
 from slo_generator.compute import compute, export, get_exporters
 from slo_generator.utils import setup_logging, parse_config
-from flask import jsonify
 
 CONFIG_URL = environ['CONFIG_URL']
 EXPORTERS_URL = environ.get('EXPORTERS_URL', None)
