b9dbbd623faf317c5b715f5b42f1874401bd2650
Kevin Labesse <kevin@labesse.me>
2021-10-13 14:59:46 +0200

    typo
    Signed-off-by: Kevin Labesse <kevin@labesse.me>

--- slo_generator/exporters/prometheus.py
+++ slo_generator/exporters/prometheus.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-`stackdriver.py`
-Stackdriver Monitoring exporter class.
+`prometheus.py`
+Prometheus Push Gateway exporter class.
 """
 import logging
 
