13d41f4fd8a4af98633275d0c3e36e8a23c21f08
mgerylrm <mathieu.gery@ext.leroymerlin.fr>
2022-09-01 14:21:25 +0000

    Fix zabbix backend divide by 100

--- slo_generator/backends/zabbix.py
+++ slo_generator/backends/zabbix.py
@@ -52,4 +52,4 @@ class ZabbixBackend:
         response = self.client.sla.getsli(serviceids=service_id, slaid=sla_id, period_from=timestamp - window, period_to=timestamp, periods=1)
         sli_value = response.get("sli")[0][0].get("sli")
         LOGGER.debug(f"SLI value: {sli_value}")
-        return sli_value
+        return sli_value/100
