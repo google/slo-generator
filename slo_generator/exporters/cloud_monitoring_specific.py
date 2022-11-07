930b64bf751794add3cb846dc1d51a5363e04148
florianmartineau <67061306+florianmartineau@users.noreply.github.com>
2022-11-07 16:56:12 +0100

    update cloud_monitoring_exporter

--- slo_generator/exporters/cloud_monitoring_specific.py
+++ slo_generator/exporters/cloud_monitoring_specific.py
@@ -17,8 +17,6 @@ Cloud Monitoring exporter class.
 """
 import logging
 
-import google.api_core.exceptions
-from google.api import metric_pb2 as ga_metric
 from google.cloud import monitoring_v3
 
 from .base import MetricsExporter
@@ -45,8 +43,6 @@ class CloudMonitoringSpecificExporter(MetricsExporter):
         Returns:
             object: Cloud Monitoring API result.
         """
-        if not self.get_metric_descriptor(data):
-            self.create_metric_descriptor(data)
         self.create_timeseries(data)
 
     def create_timeseries(self, data):
@@ -58,137 +54,50 @@ class CloudMonitoringSpecificExporter(MetricsExporter):
         Returns:
             object: Metric descriptor.
         """
-        labels = data['labels']
-        series = monitoring_v3.types.TimeSeries()
-        series.metric.type = data['name']
+        series = monitoring_v3.TimeSeries()
+        series.metric.type = data["name"]
+        series.resource.type = "global"
+        labels = data["labels"]
         for key, value in labels.items():
             if key == "slo_name" or key == "product_name" or key == "slo_type" or key == "env" or key == "product_id"  or key == "service_name" or key == "platform" or key == "client_coverage" or key == "slo_statement" or key == "feature_name" or key == "module_id" or key == "community" or key == "domain" or key == "error_budget_policy_step_name" or key == "entity":
                 series.metric.labels[key] = value
-        series.resource.type = 'global'
-
-        # Create a new data point.
-        #point = series.points.add()
-
+    
         # Define end point timestamp.
-        timestamp = data['timestamp']
+        timestamp = data["timestamp"]
         seconds = int(timestamp)
-        nanos = int((timestamp - seconds) * 10 ** 9)
-        interval = monitoring_v3.TimeInterval({
-            "end_time": {
-                "seconds": seconds,
-                "nanos": nanos
+        nanos = int((timestamp - seconds) * 10**9)
+        interval = monitoring_v3.TimeInterval(
+            {
+                "end_time": {
+                    "seconds": seconds,
+                    "nanos": nanos,
+                }
             }
-        })
+        )
 
         # Create a new data point and set the metric value.
-        point = monitoring_v3.Point({
-            "interval": interval,
-            "value": {
-                "double_value": data['value']
+        point = monitoring_v3.Point(
+            {
+                "interval": interval,
+                "value": {
+                    "double_value": data["value"],
+                },
             }
-        })
+        )
         series.points = [point]
 
         
-        """
-        point.interval.end_time.seconds = int(timestamp)
-        point.interval.end_time.nanos = int(
-            (timestamp - point.interval.end_time.seconds) * 10**9)
-
-        # Set the metric value.
-        point.value.double_value = data['value'] 
-
-        # Record the timeseries to Cloud Monitoring.
-        project = self.client.project_path(data['project_id'])
-        self.client.create_time_series(project, [series])
-        """
-        # Record the timeseries to Cloud Monitoring.
-        project = self.client.common_project_path(data['project_id'])
+        project = self.client.common_project_path(data["project_id"])
         self.client.create_time_series(name=project, time_series=[series])
         # pylint: disable=E1101
         labels = series.metric.labels
         LOGGER.debug(
-            f"timestamp: {timestamp} value: {point.value.double_value}"
+            f"timestamp: {timestamp}"
+            f"value: {point.value.double_value}"
             f"{labels['service_name']}-{labels['feature_name']}-"
-            f"{labels['slo_name']}-{labels['error_budget_policy_step_name']}")
+            f"{labels['slo_name']}-{labels['error_budget_policy_step_name']}"
+        )
 
     
 
-    """
-    def get_metric_descriptor(self, data):
-        Get Cloud Monitoring metric descriptor.
-
-        Args:
-            data (dict): Metric data.
-
-        Returns:
-            object: Metric descriptor (or None if not found).
-        
-        descriptor = self.client.metric_descriptor_path(data['project_id'],
-                                                        data['name'])
-        try:
-            return self.client.get_metric_descriptor(descriptor)
-        except google.api_core.exceptions.NotFound:
-            return None
-    """
-
-    def get_metric_descriptor(self, data):
-        """Get Cloud Monitoring metric descriptor.
-
-        Args:
-            data (dict): Metric data.
-
-        Returns:
-            object: Metric descriptor (or None if not found).
-        """
-        project_id = data['project_id']
-        metric_id = data['name']
-        request = monitoring_v3.GetMetricDescriptorRequest(
-            name=f"projects/{project_id}/metricDescriptors/{metric_id}")
-        try:
-            return self.client.get_metric_descriptor(request)
-        except google.api_core.exceptions.NotFound:
-            return None
-
-    """
-    def create_metric_descriptor(self, data):
-        Create Cloud Monitoring metric descriptor.
-
-        Args:
-            data (dict): Metric data.
-
-        Returns:
-            object: Metric descriptor.
-        
-        project = self.client.project_path(data['project_id'])
-        descriptor = monitoring_v3.types.MetricDescriptor()
-        descriptor.type = data['name']
-        descriptor.metric_kind = (
-            monitoring_v3.enums.MetricDescriptor.MetricKind.GAUGE)
-        descriptor.value_type = (
-            monitoring_v3.enums.MetricDescriptor.ValueType.DOUBLE)
-        descriptor.description = data['description']
-        self.client.create_metric_descriptor(project, descriptor)
-        return descriptor
-    """
-    
-    def create_metric_descriptor(self, data):
-        """Create Cloud Monitoring metric descriptor.
-
-        Args:
-            data (dict): Metric data.
-
-        Returns:
-            object: Metric descriptor.
-        """
-        project = self.client.common_project_path(data['project_id'])
-        descriptor = ga_metric.MetricDescriptor()
-        descriptor.type = data['name']
-        # pylint: disable=E1101
-        descriptor.metric_kind = ga_metric.MetricDescriptor.MetricKind.GAUGE
-        descriptor.value_type = ga_metric.MetricDescriptor.ValueType.DOUBLE
-        # pylint: enable=E1101
-        descriptor.description = data['description']
-        descriptor = self.client.create_metric_descriptor(
-            name=project, metric_descriptor=descriptor)
-        return descriptor
+   
