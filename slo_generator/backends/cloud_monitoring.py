8551dbb92196afbfec027ccd3b9f6a28e19ee789
florianmartineau <67061306+florianmartineau@users.noreply.github.com>
2022-12-08 17:28:07 +0100

    retry 60s

--- slo_generator/backends/cloud_monitoring.py
+++ slo_generator/backends/cloud_monitoring.py
@@ -207,8 +207,7 @@ class CloudMonitoringBackend:
         request.interval = measurement_window
         request.view = monitoring_v3.ListTimeSeriesRequest.TimeSeriesView.FULL
         request.aggregation = aggregation
-        request.timeout=60.0
-        timeseries = self.client.list_time_series(request)
+        timeseries = self.client.list_time_series(request,timeout=60.0)
         LOGGER.debug(pprint.pformat(timeseries))
         return timeseries
 
