94dab2676a1a78e298eb9f268063a2ea982d24ca
Sophian Mehboub <sophienmehboub@gmail.com>
2023-01-28 00:50:00 +0100

    ci: `make unit` fails because of a str local date does not corresponding to a timestamp

--- tests/unit/backends/test_cloud_monitoring_mql.py
+++ tests/unit/backends/test_cloud_monitoring_mql.py
@@ -15,6 +15,7 @@
 # flake8: noqa
 
 import unittest
+from datetime import datetime
 
 from slo_generator.backends.cloud_monitoring_mql import CloudMonitoringMqlBackend
 
@@ -31,13 +32,17 @@ class TestCloudMonitoringMqlBackend(unittest.TestCase):
     || metric.response_code == 200
 """
 
-        enriched_query = """fetch gae_app
+        end_time_str: str = datetime.fromtimestamp(timestamp).strftime(
+            "%Y/%m/%d %H:%M:%S"
+        )
+
+        enriched_query = f"""fetch gae_app
 | metric 'appengine.googleapis.com/http/server/response_count'
 | filter resource.project_id == 'slo-generator-demo'
 | filter
     metric.response_code == 429
     || metric.response_code == 200
-| group_by [] | within 3600s, d'2022/10/28 22:10:15' | every 3600s"""
+| group_by [] | within 3600s, d'{end_time_str}' | every 3600s"""
 
         assert (
             CloudMonitoringMqlBackend.enrich_query_with_time_horizon_and_period(
